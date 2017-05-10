ec9d5e877e8b306c64263a47e9e982eef146c65e
Zinat Wali <zwali@scottlogic.co.uk>
2017-05-10 16:14:35 +0100

    initial commit

---
+++ emr/variables.tf
@@ -0,0 +1,11 @@
+variable "aws_subnet_main_id" {
+  description = "subnet id, please provide"
+}
+
+variable "aws_security_group_sg_id" {
+  description = "security group id, please provide"
+}
+
+variable "aws_key_name" {
+  description = "key pair name, please provide"
+}
